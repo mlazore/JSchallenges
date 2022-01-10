cc23df50b23746a33f32f90e976f116bef9d5b1b
mlazore <martinlazore@gmail.com>
2022-01-10 01:21:52 -0500

    JS and html done

---
+++ hard/hard.js
@@ -0,0 +1,30 @@
+let num1 = 2;
+
+let num2 = 4;
+
+function add (num1, num2){
+    return num1 + num2;
+}
+let answer = add(num1 , num2);
+console.log(`the outcome of ${num1} + ${num2} equals ${answer}`);
+
+
+function subtract (num1 , num2){
+    return num1 - num2;
+}
+answer = subtract (num1 , num2);
+console.log(`The outcome of ${num1} - ${num2} equals ${answer}`);
+
+
+function multiply (num1 , num2){
+    return num1 * num2;
+}
+answer = multiply (num1 , num2);
+console.log(`The outcome of ${num1} * ${num2} equals ${answer}`);
+
+
+function divide (num1 , num2){
+    return num1 / num2;
+}
+answer = divide (num1 , num2);
+console.log(`The outcome of ${num1} / ${num2} equals ${answer}`);
